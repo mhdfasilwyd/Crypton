2e409536ea3c4658485054f11012f201e59f3f33
Muhammed Fasil K <mhdfasilwyd@gmail.com>
2021-08-02 00:26:15 +0530

    Build file changes

--- CoinGecko/lib/libcpr.sh
+++ CoinGecko/lib/libcpr.sh
@@ -1,10 +1,15 @@
-# Build CPR Library
+# Install OpenSSL
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install libssl-dev 
+sudo apt-get install libssl-dev
+
+# Build CPR Library
 git clone https://github.com/whoshuu/cpr
 cd cpr
 mkdir build && cd build
 cmake ..
 make
 sudo make install
+
+# Copy Library to Local
+cp -ar lib/. ../../../lib/
